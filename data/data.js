fdaf80c8d059b4076c07bb80f5deca545c7e6638
roshaninmobi <94671222+roshaninmobi@users.noreply.github.com>
2021-11-19 12:02:11 +0530

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,35 @@
+var data = {
+	devMode: true,
+	isHighlights: true,
+	details: [{
+		appName: "Roposo",
+		packageName: "com.roposo.android",
+		appBeacons: {
+			appSubmitBeacons: [""],
+			installCompleteBeacons: ["https://app.appsflyer.com/com.roposo.android?pid=glance_int&c=glance&af_adset=$GLANCE_ID&af_ad=$GLANCE_ID&af_siteid=GLNCOCI&af_channel=GLANCE&af_c_id=GLNCOCI&af_adset_id=$GLANCE_ID&af_ad_id=$GLANCE_ID&af_click_lookback=7d&advertising_id=$GPID&click_id=$IMP_ID&sha1_android=$O1"],
+			notificationBeacon: [""]
+		},
+		appDetailedInfo: {
+			brandColor: '#100118',
+			ctaBackgroundColor: "#fff",
+			ctaColor: "#1c0c4a",
+			installLaterBackgroundColor: "transparent",
+			installLaterColor: "#fff",
+
+			appLogo: "logo.svg",
+			appText:"<div style='color: rgba(255,255,255,0.7)'>Join India's coolest platform for <b style='color: white;'> LIVE </b></div><b> entertainment & shopping</b>"
+		},
+		installLater: null,
+		installButtonText:'Install Roposo',
+		installLaterButtonText:'Install Later',
+		showConfirmation: null,
+		installLaterOnUnlockCancel: null
+	}],
+	toastMessageOnline: "App will install in next unlock.",
+	toastMessageOffline: "Internet is Off",
+	rescursiveMsg1: "Install Unsuccessful",
+	rescursiveMsg2: "10cr+  users are using Roposo app.",
+	ctaBeaconUrl: "https://app.appsflyer.com/com.roposo.android?pid=glance_int&c=glance&af_adset=$GLANCE_ID&af_ad=$GLANCE_ID&af_siteid=GLNCOCI&af_channel=GLANCE&af_c_id=GLNCOCI&af_adset_id=$GLANCE_ID&af_ad_id=$GLANCE_ID&af_click_lookback=7d&advertising_id=$GPID&click_id=$IMP_ID&sha1_android=$O1",
+	installNowVisibility: true,
+	installLaterVisibility: true
+}
